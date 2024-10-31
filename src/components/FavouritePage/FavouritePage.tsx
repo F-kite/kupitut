fa9488cb6083de5d709d53ab536c122ce4e6635a
F-kite <ilichevv_v845@mail.ru>
2024-10-31 17:57:01 +0700

    31.10 (localstorage)

--- src/components/FavouritePage/FavouritePage.tsx
+++ src/components/FavouritePage/FavouritePage.tsx
@@ -1,5 +1,17 @@
+"use client";
+import { useEffect, useState } from "react";
 import styles from "../productCard/styles.module.scss";
 
 export default function FavouritePage({}) {
-  return <div>Это страница с избранными продуктами</div>;
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  if (loading) {
+    return <p>Загрузка...</p>;
+  }
+  return <div>Страница с избранными продуктами</div>;
 }
--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -1,31 +1,70 @@
 "use client";
 import { useSearchParams } from "next/navigation";
 import { useProducts } from "@/hooks/useProducts";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "../productCard/styles.module.scss";
 
+/*
+Название: {product.name}
+Продавец: {product.seller}
+Цена: {product.price}
+Информация: {product.info}
+Характеристика: {product.specifications} Пункты разделены символом ';'
+Фотография: {product.image}
+Количество: {product.quantity}
+*/
+
 export default function ProductPage({}) {
-  const searchParams = useSearchParams();
   const { products, getProducts } = useProducts();
+  const searchParams = useSearchParams();
+  const [loading, setLoading] = useState(true);
+  const [isToFavourite, setIsToFavourite] = useState(false);
+  const [isToCart, setIsToCart] = useState(false);
 
-  const id = searchParams.get("id");
+  const id: number = Number(searchParams.get("id"));
+  const product = id ? products[id - 1] : null;
 
-  const product = products[Number(id) - 1];
-  console.log(product.productname);
+  console.debug(products);
 
   useEffect(() => {
-    getProducts();
+    const fetchData = async () => {
+      await getProducts();
+      setLoading(false);
+    };
+    fetchData();
   }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined" && product) {
+      setIsToFavourite(localStorage.idFavProducts?.includes(product?.id));
+      setIsToCart(localStorage.idCartProducts?.includes(product?.id));
+    }
+  }, [product]);
+
+  if (loading) {
+    return <p>Загрузка...</p>;
+  }
 
   return (
     <div>
-      Страница продукта
-      <p>{product.id}</p>
-      <p>{product.productname}</p>
-      <p>{product.productseller}</p>
-      <p>{product.productcost}</p>
-      <p>{product.productspecifications}</p>
-      <p>{product.productimage}</p>
+      <p>Карточка товара</p>
+      {product ? (
+        <ul>
+          <li>Название: {product.name}</li>
+          <li>Цена: {product.price}</li>
+          <li>Продавец: {product.seller}</li>
+          <li>Информация: {product.info}</li>
+          <li>Характеристика: {product.specifications}</li>
+          <li>Фотография: {product.image}</li>
+          <li>Количество: {product.quantity}</li>
+        </ul>
+      ) : (
+        <p>Товар не найден</p>
+      )}
+      <ul>
+        <li>В избранном {isToFavourite.toString()}</li>
+        <li>В корзине {isToCart.toString()}</li>
+      </ul>
     </div>
   );
 }
